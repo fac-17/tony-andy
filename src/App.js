e26d476846e7ea6982200a225331c1949061999d
andy <andy.william.mcdonald@gmail.com>
2019-09-25 15:36:01 +0100

    randomized api information

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from "react";
 import logo from "./logo.svg";
 import { Userimage } from "./components/userImage/index";
 import "./App.css";
+import { getGithub } from "./utils/utils";
+import randomizer from "./utils/randomiser";
 
-function App() {
-  return <Userimage />;
-}
+
+
+  function App() {
+    const [gameData, setGameData] = React.useState(null)
+
+    React.useEffect(   () => {
+      getGithub().then(data => setGameData(data))
+    }, [])
+    if (!gameData) {
+      return <h3>...Loading</h3>
+    }
+
+    console.log(gameData[randomizer(gameData.length)]);
+    
+  
+    // getGithub().then(data =>  )
+
+
+    return <Userimage />;
+
+
+  }
 
 export default App;
--- src/components/userImage/index.js
+++ src/components/userImage/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import {getGithub} from "../../utils/utils"
 
 export const Userimage = ({ imgSrc, userName }) => {
+
+
   return (
     <img
       src="https://avatars3.githubusercontent.com/u/39386043?v=4"
