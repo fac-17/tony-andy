181b2334ae31cdf273b56181b1064c1df414a4ad
Tony Lomax <antl.lomax@gmail.com>
2019-09-25 16:01:08 +0100

    Link api call to render of user images

--- src/App.js
+++ src/App.js
@@ -5,27 +5,19 @@ import "./App.css";
 import { getGithub } from "./utils/utils";
 import randomizer from "./utils/randomiser";
 
+function App() {
+  const [gameData, setGameData] = React.useState(null);
 
-
-  function App() {
-    const [gameData, setGameData] = React.useState(null)
-
-    React.useEffect(   () => {
-      getGithub().then(data => setGameData(data))
-    }, [])
-    if (!gameData) {
-      return <h3>...Loading</h3>
-    }
-
-    console.log(gameData[randomizer(gameData.length)]);
-    
-  
-    // getGithub().then(data =>  )
-
-
-    return <Userimage />;
-
-
+  React.useEffect(() => {
+    getGithub().then(data => setGameData(data));
+  }, []);
+  if (!gameData) {
+    return <h3>...Loading</h3>;
   }
+
+  const { login, avatar_url } = gameData[randomizer(gameData.length)];
+
+  return <Userimage imgSrc={avatar_url} />;
+}
 
 export default App;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,12 @@
-function getGithub(){
-    return fetch(`https://api.github.com/orgs/fac-17/members?access_token=2017e4a6f02ddef92d8c23701428863ee222786a`)
+function getGithub() {
+  return fetch(
+    `https://api.github.com/orgs/fac-17/members?access_token=${process.env.REACT_APP_token}`
+  )
     .then(res => {
-        if (!res.ok) throw new Error ("There is an error");
-        return res;
+      if (!res.ok) throw new Error("There is an error");
+      return res;
     })
-    .then(res => res.json()); 
+    .then(res => res.json());
 }
 
 module.exports = { getGithub };
-
