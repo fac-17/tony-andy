c07f7a729b14c8f09927be60b1216e23a9995a17
Tony Lomax <antl.lomax@gmail.com>
2019-09-25 12:48:16 +0100

    Check that dummy data works in render of user image

---
+++ src/components/userImage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Userimage = ({ imgSrc, userName }) => {
+  return (
+    <img
+      src="https://avatars3.githubusercontent.com/u/39386043?v=4"
+      alt="testimg"
+    ></img>
+  );
+};
