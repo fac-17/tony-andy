de40ab1ed2584fae81d5edab56521c1385fa32ab
Tony Lomax <antl.lomax@gmail.com>
2019-09-26 16:07:55 +0100

    Fix timer re-rendering everything

--- src/App.js
+++ src/App.js
@@ -7,36 +7,52 @@ import { getGithub } from "./utils/utils";
 import randomizer from "./utils/randomiser";
 import Clock from "./components/clock /clock";
 
-
 function App() {
   const [gameData, setGameData] = React.useState(null);
   const [count, setCount] = React.useState(0);
   const [seconds, setSeconds] = React.useState(30);
   const [active, setActive] = React.useState(true);
+  const [randomName1, nameUpdater1] = React.useState("");
+  const [randomName2, nameUpdater2] = React.useState("");
+  const [answer, answerUpdater] = React.useState("");
 
   React.useEffect(() => {
     getGithub().then(data => setGameData(data));
   }, []);
-  if (!gameData) {
+
+  React.useEffect(() => {
+    if (gameData) {
+      nameUpdater1(gameData[randomizer(gameData.length)].login);
+      nameUpdater2(gameData[randomizer(gameData.length)].login);
+      answerUpdater(gameData[randomizer(gameData.length)]);
+    }
+  }, [count, gameData]);
+
+  if (!gameData || !answer) {
     return <h3>...Loading</h3>;
   }
 
-  const { login, avatar_url } = gameData[randomizer(gameData.length)];
-
-  const randomName1 = gameData[randomizer(gameData.length)].login;
-
-  const randomName2 = gameData[randomizer(gameData.length)].login;
-
+  const { login, avatar_url } = answer;
 
   return (
     <section>
-      <Userimage imgSrc={avatar_url}/>
-      <UserNames count={count} setCount={setCount} userNames={login} randomName1 = {randomName1} randomName2 = {randomName2}/>
-      <Clock seconds = {seconds} setSeconds = {setSeconds} active={active} setActive={setActive} />
-    </section>
-    
-  )
+      <Userimage imgSrc={avatar_url} />
+      <UserNames
+        count={count}
+        setCount={setCount}
+        userNames={login}
+        randomName1={randomName1}
+        randomName2={randomName2}
+      />
 
+      <Clock
+        seconds={seconds}
+        setSeconds={setSeconds}
+        active={active}
+        setActive={setActive}
+      />
+    </section>
+  );
 }
 
 export default App;
--- src/components/userName/index.js
+++ src/components/userName/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { shuffler } from "../../utils/shuffler";
 
-export const UserNames = ({ userNames, randomName1, randomName2, setCount, count }) => {
-  const newArray = shuffler([userNames, randomName1, randomName2]);
+export const UserNames = ({
+  userNames,
+  randomName1,
+  randomName2,
+  setCount,
+  count
+}) => {
+  const newArray = React.useMemo(() => {
+    return shuffler([userNames, randomName1, randomName2]);
+  }, [userNames, randomName1, randomName2]);
 
   const handleClick = value => {
-    if(value === userNames){
-      setCount(count +1)
+    if (value === userNames) {
+      setCount(count + 1);
     }
   };
-
+  console.log(newArray);
   return (
     <div>
       <p>{count}</p>
       {newArray.map((e, i) => (
-        <label htmlFor={e} key={e[i]}>
-          <button onClick={()=> handleClick(e)}>
-          {e}</button>
+        <label htmlFor={e} key={e}>
+          <button onClick={() => handleClick(e)}>{e}</button>
         </label>
       ))}
     </div>
