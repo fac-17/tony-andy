095fc792b206ea0688e8890d1f049c683b809d1d
andy <andy.william.mcdonald@gmail.com>
2019-09-27 12:37:10 +0100

    randomizer updated and actually appears to be working now

--- src/utils/randomiser.js
+++ src/utils/randomiser.js
@@ -1,11 +1,15 @@
 const randomizer = (max) => {
-    let num = Math.random()
-    if(num === 0) {
-      return num
-    }
-    else {
-      return Math.floor((num * max) +1 )
-    }
+  let numArray = []
+  while (numArray.length < 3) {
+  let num = Math.random()
+  if(num === 0 && (!numArray.includes(num))) {
+    numArray.push(num)
   }
+  else if (!numArray.includes(Math.floor((num * max) +1))) {
+    numArray.push(Math.floor((num * max) +1))
+  }
+  }
+  return numArray
+}
 
- export default randomizer
+export default randomizer;
